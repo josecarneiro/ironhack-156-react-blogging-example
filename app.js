0ce1741fe7cae76de372626dda19953b985b52c6
José Carneiro <josemcarneiro@gmail.com>
2019-09-30 09:39:39 +0100

    First commit in separate repo

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const { join } = require("path");
+const express = require("express");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const serveFavicon = require("serve-favicon");
+
+// const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(serveFavicon(join(__dirname, "public/images", "favicon.ico")));
+app.use(express.static(join(__dirname, "client/build")));
+
+const path = require('path');
+
+// app.use('/', indexRouter);
+app.use("/api", apiRouter);
+
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+// Catch missing routes and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Catch all error handler
+app.use((error, req, res, next) => {
+  // Set error information, with stack only available in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get("env") === "development" ? error : {};
+
+  res.status(error.status || 500);
+  res.json({ type: "error", error: { message: error.message } });
+});
+
+module.exports = app;
